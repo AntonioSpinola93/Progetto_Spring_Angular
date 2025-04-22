b4cc7055144070f61eeb6aa31f37c73898b5bd80
Pana98 <Zeus19998@gmail.com>
2025-04-22 16:04:13 +0000

    Fix logout

--- angular-15-client/src/app/log-out/log-out.component.ts
+++ angular-15-client/src/app/log-out/log-out.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { LoginService } from '../login.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-log-out',
@@ -8,10 +9,15 @@ import { LoginService } from '../login.service';
 })
 export class LogOutComponent {
   constructor(
-    private loginService: LoginService
-  ) { }
+    private loginService: LoginService,
+    private router: Router
+  ) {
+    this.logout(); // Esegui logout appena entri nel componente
+  }
+
   logout() {
-          this.loginService.updateLoginStatus('failure');
-          localStorage.removeItem('isLoggedIn');
-         }
+    this.loginService.updateLoginStatus('failure');
+    localStorage.removeItem('isLoggedIn');
+    this.router.navigate(['/login']); // Redirect al login
+  }
 }
